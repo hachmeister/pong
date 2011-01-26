00a966550aaa259ce854f7f6fb2ba857f187b723
Karsten Hachmeister <k.hachmeister@googlemail.com>
2011-01-26 15:46:52 +0100

    Kollision überarbeitet und Bewegung des Computergegners

--- src/playstate.cc
+++ src/playstate.cc
@@ -80,6 +80,8 @@ void PlayState::compute()
 
 float PlayState::update_ball_pos(float delta)
 {
+  //std::cout << "--------\n";
+
   float delta_for_update = delta;
   bool collision = false;
 
@@ -92,27 +94,55 @@ float PlayState::update_ball_pos(float delta)
 
   /*
   std::cout << "old x: " << ball_pos_x << "; old y: " << ball_pos_y << "\n";
-  std::cout << "new x: " << new_ball_pos_x << "; new y: " << new_ball_pos_y << "\n";
+  std::cout << "temp x: " << new_ball_pos_x << "; temp y: " << new_ball_pos_y << "\n";
   std::cout << "delta_x: " << fabs(new_ball_pos_x - ball_pos_x) << "; delta_y: " << fabs(new_ball_pos_y - ball_pos_y) << "\n";
   std::cout << "delta2_x: " << (312.0f - fabs(ball_pos_x)) << "; delta2_y: " << (224.0f - fabs(ball_pos_y)) << "\n";
   */
 
   if (ball_pos_x >= -312.0f && new_ball_pos_x < -312.0f && delta_x > 0.0f) {
-    delta_for_update = delta * (312.0f - fabs(ball_pos_x)) / delta_x;
-    new_angle = 180.0f - ball_angle_;
-    collision = true;
-  } else if (ball_pos_x <= 312.0f && new_ball_pos_x > 312.0f && delta_x > 0.0f) {
-    delta_for_update = delta * (312.0f - fabs(ball_pos_x)) / delta_x;
-    new_angle = 180.0f - ball_angle_;
-    collision = true;
-  } else if (ball_pos_y >= -224.0f && new_ball_pos_y < -224.0f && delta_y > 0.0f) {
-    delta_for_update = delta * (224.0f - fabs(ball_pos_y)) / delta_y;
-    new_angle = 360.0f - ball_angle_;
-    collision = true;
-  } else if (ball_pos_y <= 224.0f && new_ball_pos_y > 224.0f && delta_y > 0.0f) {
-    delta_for_update = delta * (224.0f - fabs(ball_pos_y)) / delta_y;
-    new_angle = 360.0f - ball_angle_;
-    collision = true;
+    float temp_delta_for_update = delta * (312.0f - fabs(ball_pos_x)) / delta_x;
+    if (temp_delta_for_update < delta_for_update) {
+      float temp_ball_pos_y = ball_pos_y + (ball_speed_y * temp_delta_for_update);
+      float ball_distance = fabs(fabs(paddle1 + 400.0f) - fabs(temp_ball_pos_y + 400.0f));
+      /*
+      std::cout << "    ball: " << ball_pos_y << " + (" << ball_speed_y << " * " << temp_delta_for_update << ") = " << temp_ball_pos_y << " (" << int(temp_ball_pos_y) << ")\n";
+      std::cout << "  paddle: " << paddle1 << " (" << int(paddle1) << ")\n";
+      std::cout << "distance: " << ball_distance << "\n";
+      */
+      if (ball_distance < 40.0f) {
+        delta_for_update = temp_delta_for_update;
+        new_angle = 180.0f - ball_angle_;
+        collision = true;
+      }
+    }
+  }
+  if (ball_pos_x <= 312.0f && new_ball_pos_x > 312.0f && delta_x > 0.0f) {
+    float temp_delta_for_update = delta * (312.0f - fabs(ball_pos_x)) / delta_x;
+    if (temp_delta_for_update < delta_for_update) {
+      float temp_ball_pos_y = ball_pos_y + (ball_speed_y * temp_delta_for_update);
+      float ball_distance = fabs(fabs(paddle2 + 400.0f) - fabs(temp_ball_pos_y + 400.0f));
+      if (ball_distance < 40.0f) {
+        delta_for_update = temp_delta_for_update;
+        new_angle = 180.0f - ball_angle_;
+        collision = true;
+      }
+    }
+  }
+  if (ball_pos_y >= -224.0f && new_ball_pos_y < -224.0f && delta_y > 0.0f) {
+    float temp_delta_for_update = delta * (224.0f - fabs(ball_pos_y)) / delta_y;
+    if (temp_delta_for_update < delta_for_update) {
+      delta_for_update = temp_delta_for_update;
+      new_angle = 360.0f - ball_angle_;
+      collision = true;
+    }
+  }
+  if (ball_pos_y <= 224.0f && new_ball_pos_y > 224.0f && delta_y > 0.0f) {
+    float temp_delta_for_update = delta * (224.0f - fabs(ball_pos_y)) / delta_y;
+    if (temp_delta_for_update < delta_for_update) {
+      delta_for_update = temp_delta_for_update;
+      new_angle = 360.0f - ball_angle_;
+      collision = true;
+    }
   }
 
   //std::cout << "delta_for_update: " << delta_for_update << "\n";
@@ -120,14 +150,15 @@ float PlayState::update_ball_pos(float delta)
   ball_pos_x += ball_speed_x * delta_for_update;
   ball_pos_y += ball_speed_y * delta_for_update;
 
-  //std::cout << "old angle: " << ball_angle_ << "\n";
+  //std::cout << "new x: " << ball_pos_x << "; new y: " << ball_pos_y << "\n";
 
   if (collision) {
     angle(new_angle);
     compute();
   }
 
-  //std::cout << "new angle: " << ball_angle_ << "\n";
+  // computer paddle
+  paddle2 += ((ball_pos_y + 300.0f) - (paddle2 + 300.0f)) * pow((ball_pos_x + 400.0f) / 1200.0f, 2.5f);
 
   return delta - delta_for_update;
 }
